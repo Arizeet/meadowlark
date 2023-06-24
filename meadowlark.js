0821b5b4fc9c6925b3ecf9ee245d5a3237f6ee7b
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2023-06-24 16:17:38 +0530

    Moved 'fortune cookies' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var app = express();
+const fortune = require('./lib/fortune.js');
 
 //set up handlebars view engine
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
 
 app.set('port', process.env.PORT || 3000); //'port' -> port
 
@@ -22,8 +16,7 @@ app.get('/',function(req,res){
     res.render('home');
 });
 app.get('/about',function(req,res){
-    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about',{fortune:randomFortune});
+    res.render('about',{fortune:fortune.getFortune()});
 });
 
 //custom 404 page
